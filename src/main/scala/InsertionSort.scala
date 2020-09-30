eb6a506d1455936c391441755c9ca0a492319a38
chsprithvi <chsprithvi@gmail.com>
2020-09-30 15:47:45 +0530

    [Siddhartha]Refactored:Magic Numbers

--- src/main/scala/InsertionSort.scala
+++ src/main/scala/InsertionSort.scala
@@ -2,33 +2,34 @@ import scala.io.StdIn.readLine
 
 object InsertionSort {
   def Isort(arr: Array[String]): Unit = {
-    val n = arr.length
-    for (i <- 1 until n) {
-      val key = arr(i)
-      var j = i - 1
-      /* Move elements of arr[0..i-1], that are
-                     greater than key, to one position ahead
-                     of their current position */
-      while (j >= 0 && arr(j) > key) {
-        arr(j + 1) = arr(j)
-        j = j - 1
+    val number2 = arr.length
+    for (loopIntial <- 1 until number2) {
+      val key = arr(loopIntial)
+      var loopSecond = loopIntial - 1
+      /* Move elements of arr[0..loopArray-1], that are
+        greater than key, to one position ahead
+        of their current position 
+      */
+      while (loopSecond >= 0 && arr(loopSecond) > key) {
+        arr(loopSecond + 1) = arr(loopSecond)
+        loopSecond = loopSecond - 1
       }
-      arr(j + 1) = key
+      arr(loopSecond + 1) = key
     }
   }
 
-  /* A utility function to print array of size n*/
+  /* A utility function to print array of size number2*/
   def printArray(arr: Array[String]): Unit = {
-    val n = arr.length
-    for (i <- 0 until n) {
-      print(arr(i) + " ")
+    val number2 = arr.length
+    for (loopArray <- 0 until number2) {
+      print(arr(loopArray) + " ")
     }
     println()
   }
   def main(args: Array[String]): Unit = {
     val number1=readLine("Enter the number of strings you want to input:").toInt
     val arr=new Array[String](number1)
-    for (i <- 0 until(number1)){arr(i)=readLine(s"String$i")}
+    for (loopArray <- 0 until(number1)){arr(loopArray)=readLine(s"String$loopArray")}
     Isort(arr)
     printArray(arr)
   }

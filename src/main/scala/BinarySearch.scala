f1a8d6e383e414954276e7903465e846d224cf9b
chsprithvi <chsprithvi@gmail.com>
2020-09-30 13:41:33 +0530

    [Siddhartha]Added:file creation and appending to Array

---
+++ src/main/scala/BinarySearch.scala
@@ -0,0 +1,41 @@
+import java.io.{File, PrintWriter}
+
+import scala.io.Source
+import scala.io.StdIn.readLine
+
+object BinarySearch {
+
+  def main(args: Array[String]): Unit = {
+    var token1 = ""
+    val temps = new Array[String](6)
+    try{
+      // Creating a file
+      val file_Object = new File("abc.txt" )
+      // Passing reference of file to the printwriter
+      val print_Writer = new PrintWriter(file_Object)
+      // Writing to the file
+      print_Writer.write("Hello,This,is,Geeks,For,Geeks,")
+      // Closing printwriter
+      print_Writer.close()
+      var newString = ""
+      val fname = new File("abc.txt")
+      val fSource = Source.fromFile(fname)
+      var i = 0
+      while (fSource.hasNext) {
+        token1=fSource.next.toString
+        if (!token1.equals(",")){
+          newString+=token1
+        }
+        else{
+          temps(i)=newString
+          newString=""
+          i+=1
+        }
+      }
+      fSource.close()
+    }
+    catch {
+      case _:java.io.FileNotFoundException=>println("file not found")
+    }
+  }
+}

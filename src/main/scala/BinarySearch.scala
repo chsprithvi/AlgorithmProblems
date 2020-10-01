813e908982a806261f3b1df5ddecfaf6d722f14b
chsprithvi <chsprithvi@gmail.com>
2020-10-01 16:27:24 +0530

    [Siddhartha]Refactored:Following proper coding style

--- src/main/scala/BinarySearch.scala
+++ src/main/scala/BinarySearch.scala
@@ -4,44 +4,54 @@ import scala.io.Source
 import scala.io.StdIn.readLine
 
 object BinarySearch {
-
-  def main(args: Array[String]): Unit = {
-    var token1 = ""
-    val temps = new Array[String](6)
+  val myArray = new Array[String](7)
+  def fileToArray:Unit = {
+    var letterInFile = ""
     try{
-      // Creating a file
-      val file_Object = new File("abc.txt" )
-      // Passing reference of file to the printwriter
-      val print_Writer = new PrintWriter(file_Object)
-      // Writing to the file
-      print_Writer.write("Hello,This,is,Geeks,For,Geeks,")
-      // Closing printwriter
-      print_Writer.close()
-      var newString = ""
-      val fname = new File("abc.txt")
-      val fSource = Source.fromFile(fname)
-      var i = 0
-      while (fSource.hasNext) {
-        token1=fSource.next.toString
-        if (!token1.equals(",")){
-          newString+=token1
+      val fileObject = new File("abc.txt" )
+      val printWriter = new PrintWriter(fileObject)
+      printWriter.write("Hello,Apple,a,day,keeps,doctor,away,")
+      printWriter.close()
+      var newWord = ""
+      val filename = new File("abc.txt")
+      val fileSource = Source.fromFile(filename)
+      var arrayLocation = 0
+      while (fileSource.hasNext) {
+        letterInFile=fileSource.next.toString
+        if (!letterInFile.equals(",")){
+          newWord+=letterInFile
         }
         else{
-          temps(i)=newString
-          newString=""
-          i+=1
+          myArray(arrayLocation)=newWord
+          newWord=""
+          arrayLocation+=1
         }
       }
-      fSource.close()
-      for(i<-temps) println(i)
-      println("******************")
-      val sortedArray=temps.sorted
-      for(i<-sortedArray) println(i)
-      val checkName=readLine("Enter a name to check if present or not:")
-      if (sortedArray.contains(checkName)){println(s"The ->$checkName<- is present in the file $fname")}
+      fileSource.close()
+      println("After appending strings from file to array")
+      for(arrayLocation<-myArray) println(arrayLocation)
     }
     catch {
       case _:java.io.FileNotFoundException=>println("file not found")
     }
+  }
+
+  def binarySearch(sortArray:Array[String],checkName:String): Unit = {
+    for(arrayLocation<-sortArray){
+      if(checkName.equals(arrayLocation)){
+        println(s"$checkName is present in the list")
+        return
+      }
+    }
+    println(s"$checkName not found")
+  }
+
+  def main(args: Array[String]): Unit = {
+    fileToArray
+    val sortedArray=myArray.sorted
+    println("$$$$$$$$$ After sorting $$$$$$$$$$")
+    for(arrayLocation<-sortedArray) println(arrayLocation)
+    val checkName=readLine("Enter a name to check if present or not:")
+    binarySearch(sortedArray,checkName)
   }
 }

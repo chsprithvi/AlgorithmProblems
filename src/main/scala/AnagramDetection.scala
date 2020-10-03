3e2c658613df0ffcdbdb7f42aa6ee4f813650292
chsprithvi <chsprithvi@gmail.com>
2020-10-03 14:36:54 +0530

    [Siddhartha]Refactored:removed unwanted statements

--- src/main/scala/AnagramDetection.scala
+++ src/main/scala/AnagramDetection.scala
@@ -2,25 +2,20 @@ import scala.io.StdIn.readLine
 
 object AnagramDetection {
   // function to check whether two strings are anagram of each other
-   def areAnagram(inputString: Array[Char], checkString: Array[Char]): Boolean = { // Get lenghts of both strings
+   def areAnagram(inputString: List[Char], checkString: List[Char]): Boolean = {
     val inputLength = inputString.length
     val checkLength = checkString.length
-    // If length of both strings is not same,
-    // then they cannot be anagram
     if (inputLength != checkLength) return false
-    // Sort both strings
-    val inputStringGiven=inputString.toList.sorted
-    val checkStringGiven=checkString.toList.sorted
     // Compare sorted strings
     for (i <- 0 until inputLength) {
-      if (inputStringGiven(i) != checkStringGiven(i)) return false
+      if (inputString(i) != checkString(i)) return false
     }
     true
   }
 
   def main(args: Array[String]): Unit = {
-    val inputString = readLine("Enter a word:").toCharArray
-    val checkString = readLine("Enter to check:").toCharArray
+    val inputString = readLine("Enter a word:").toCharArray.toList.sorted
+    val checkString = readLine("Enter to check:").toCharArray.toList.sorted
     if (areAnagram(inputString, checkString)) println("The two strings are anagram of each other")
     else println("The two strings are not anagram of each other")
   }
